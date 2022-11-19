f7e13ebe66c57117bed9df1e6d9161bf66ced7e6
Manish <manishkumar0156@gmail.com>
2022-11-19 19:58:37 +0530

    [Manish] Added: Cross browser in selenium

---
+++ src/test/java/com/cross/browser/test/MultiBrowser.java
@@ -0,0 +1,71 @@
+package com.cross.browser.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class MultiBrowser {
+
+	static {
+		System.setProperty("webdriver.gecko.driver",
+				"C:\\Users\\kmani\\Downloads\\geckodriver-v0.32.0-win64\\geckodriver.exe");
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\kmani\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		System.setProperty("webdriver.edge.driver", "C:\\Users\\kmani\\Downloads\\edgedriver_win32\\msedgedriver.exe");
+
+	}
+	public WebDriver driver;
+
+	@Parameters("browser")
+
+	@BeforeClass
+
+	// Passing Browser parameter from TestNG xml
+
+	public void beforeTest(String browser) {
+
+		// If the browser is Firefox, then do this
+
+		if (browser.equalsIgnoreCase("firefox")) {
+
+			// Initializing the firefox driver (Gecko)
+			driver = new FirefoxDriver();
+
+		} else if (browser.equalsIgnoreCase("chrome")) {
+
+			// Initialize the chrome driver
+
+			driver = new ChromeDriver();
+
+		} else if (browser.equalsIgnoreCase("Edge")) {
+			driver = new EdgeDriver();
+		}
+
+		// Enter the website address in the browser
+
+		driver.get("https://www.facebook.com");
+
+	}
+
+	// Once Before method is completed, Test method will start
+
+	@Test
+	public void login() throws InterruptedException {
+
+		driver.findElement(By.xpath("//button[contains(text(),'Log')]")).click();
+
+	}
+
+	@AfterClass
+	public void afterTest() {
+
+		driver.quit();
+
+	}
+}
